c702c037006ab5bc3ae5e8c385829bb29325f068
katamatata <51786805+katamatata@users.noreply.github.com>
2020-05-07 23:28:17 +0200

    Delete old components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 
-import ".App.css";
-// import About from "./components/About";
-// import Projects from "./components/Projects";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { content } from "../../content";
 
 const App = () => {
-  // const { title, aboutMe } = content;
 
   return (
-    <div className="app">
-      <nav className="navbar">
-        <div className="image-wrapper">
+    <div>
+      <nav>
+        <div>
           <img src="" alt=""/>
         </div>
-        <div className="navbar-wrapper">
+        <div>
           <ul>
             <li>About</li>
             <li>Projects</li>
@@ -27,18 +23,7 @@ const App = () => {
       </nav>
       
       <main>
-        <Header text={title} />
-        <AboutMe content={aboutMe} />
-        <Projects />
-        <section className="education">
-          <h1 className="title">Education</h1>
-        </section>
-        <section className="skills">
-          <h1 className="title">Skills</h1>
-        </section>
-        <section className="contact">
-          <h1 className="title">Contact</h1>
-        </section>
+       
       </main>
     </div>
   );
